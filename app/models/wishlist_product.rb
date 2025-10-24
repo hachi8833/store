03a7b1408287127d3eb695ba0143c46ecfab8580
hachi8833 <hachi8833@gmail.com>
2025-10-24 18:30:04 +0900

    Implement wishlist, counter-cache and friendly-url

--- app/models/wishlist_product.rb
+++ app/models/wishlist_product.rb
@@ -1,4 +1,6 @@
 class WishlistProduct < ApplicationRecord
-  belongs_to :product
-  belongs_to :wishlist
+  belongs_to :product, counter_cache: :wishlists_count
+  belongs_to :wishlist, counter_cache: :products_count
+
+  validates :product_id, uniqueness: { scope: :wishlist_id }
 end
