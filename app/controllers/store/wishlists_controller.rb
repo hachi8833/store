858d8ebe0e989ca6aa660c18ac03571f6d0b05ae
hachi8833 <hachi8833@gmail.com>
2025-10-24 19:21:56 +0900

    Add filter for admin's wishlists

--- app/controllers/store/wishlists_controller.rb
+++ app/controllers/store/wishlists_controller.rb
@@ -1,6 +1,8 @@
 class Store::WishlistsController < Store::BaseController
   def index
     @wishlists = Wishlist.includes(:user)
+    @wishlists = @wishlists.where(user_id: params[:user_id]) if params[:user_id].present?
+    @wishlists = @wishlists.includes(:wishlist_products).where(wishlist_products: { product_id: params[:product_id] }) if params[:product_id].present?
   end
 
   def show
