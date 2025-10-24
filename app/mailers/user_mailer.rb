4d85e7f3d1f1f16efc6151aa27dd4948d8b0ef58
hachi8833 <hachi8833@gmail.com>
2025-10-24 15:00:06 +0900

    Implement views for user-confirmation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,8 +5,7 @@ class UserMailer < ApplicationMailer
   #   en.user_mailer.email_confirmation.subject
   #
   def email_confirmation
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+    @token = params[:user].generate_token_for(:email_confirmation)
+    mail to: params[:user].unconfirmed_email
   end
 end
