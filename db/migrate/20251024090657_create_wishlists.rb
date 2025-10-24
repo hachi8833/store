de84b11ea9347eaf8ab6c527fce7e1c89a82c7fa
hachi8833 <hachi8833@gmail.com>
2025-10-24 18:10:07 +0900

    Update migrations

--- db/migrate/20251024090657_create_wishlists.rb
+++ db/migrate/20251024090657_create_wishlists.rb
@@ -3,7 +3,7 @@ class CreateWishlists < ActiveRecord::Migration[8.1]
     create_table :wishlists do |t|
       t.belongs_to :user, null: false, foreign_key: true
       t.string :name
-      t.integer :products_count
+      t.integer :products_count, default: 0
 
       t.timestamps
     end
--- db/migrate/20251024090842_add_wishlists_count_to_products.rb
+++ db/migrate/20251024090842_add_wishlists_count_to_products.rb
@@ -1,5 +1,5 @@
 class AddWishlistsCountToProducts < ActiveRecord::Migration[8.1]
   def change
-    add_column :products, :wishlists_count, :integer
+    add_column :products, :wishlists_count, :integer, default: 0
   end
 end
