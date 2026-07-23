6b7f7c91f17cbdf53da5dbea39f65545084e04db
hachi8833 <hachi8833@gmail.com>
2026-07-23 09:54:00 +0900

    Update Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,10 @@
 class Product < ApplicationRecord
   include Notifications
 
+  has_many :reviews, dependent: :destroy
+  has_many :subscribers, dependent: :destroy
   has_many :wishlist_products, dependent: :destroy
   has_many :wishlists, through: :wishlist_products
-  has_many :subscribers, dependent: :destroy
   has_one_attached :featured_image
   has_rich_text :description
 
