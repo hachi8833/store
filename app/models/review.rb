839282723a6cc703bb0785696451985ad8e26797
hachi8833 <hachi8833@gmail.com>
2026-07-23 09:52:51 +0900

    Update Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,8 @@
 class Review < ApplicationRecord
-  belongs_to :product
+  belongs_to :product, counter_cache: true
   belongs_to :user
   has_many_attached :images
+
+  validates :body, presence: true
+  validates :rating, presence: true, numericality: { in: 1..5, only_integer: true }
 end
