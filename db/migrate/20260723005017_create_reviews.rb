9f34f24ec4f6197806bd82465a4f49267c9163ed
hachi8833 <hachi8833@gmail.com>
2026-07-23 09:51:35 +0900

    Update migration for adding columns

--- db/migrate/20260723005017_create_reviews.rb
+++ db/migrate/20260723005017_create_reviews.rb
@@ -3,10 +3,13 @@ class CreateReviews < ActiveRecord::Migration[8.1]
     create_table :reviews do |t|
       t.belongs_to :product, null: false, foreign_key: true
       t.belongs_to :user, null: false, foreign_key: true
-      t.integer :rating
-      t.text :body
+      t.integer :rating, null: false
+      t.text :body, null: false
 
       t.timestamps
     end
+
+    add_column :products, :reviews_count, :integer, default: 0
+    add_column :products, :rating, :decimal, precision: 2, scale: 1, default: 0
   end
 end
